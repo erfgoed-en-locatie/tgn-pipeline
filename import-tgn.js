fd2b2800348a535e60a44d66bcd80da85441cb31
Bert Spaan <bert.spaan@gmail.com>
2014-11-14 16:51:10 +0100

    import script to query and download TGN data finished

---
+++ import-tgn.js
@@ -0,0 +1,19 @@
+var SparqlClient = require('sparql-client'),
+    fs = require('fs'),
+    util = require('util');
+
+var sparqlFile = 'tgn.sparql',
+    sparqlEndpoint = 'http://vocab.getty.edu/sparql.rdf';
+    //sparqlEndpoint = 'http://erfgoedenlocatie.cloud.tilaa.com/sparql';
+
+fs.readFile(sparqlFile, 'utf8', function (err, sparqlQuery) {
+  // TODO: supply SparqlClient with options to ensure application/json output
+  // For now, we have edited SparqlClient's client.js and modified mime-types there.
+  var client = new SparqlClient(sparqlEndpoint);
+
+  client.query(sparqlQuery)
+    .execute(function(error, results) {
+      //process.stdout.write(util.inspect(results, null, 20, true) + "\n");
+      fs.writeFile('tgn.xml', results, null);
+    });
+});
